afccfd1f580d606b9b0c19bb33f65b94cd81a106
Stefanía Bergljót <stefaniabergljot@gmail.com>
2023-11-08 23:20:30 +0000

    Centipede hitting logic

--- heimadaemi/verkefni3/centipede.js
+++ heimadaemi/verkefni3/centipede.js
@@ -196,7 +196,8 @@ class Projectile {
 		this.z = z;
 		this.id = id;
 		this.random = Math.random();
-		var geom = new THREE.SphereGeometry(0.2*square, 12, 12);
+		var radius = 0.2*square;
+		var geom = new THREE.SphereGeometry(radius, 12, 12);
 		const material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide});
 		material.color.setHex(0xffa500);
 		const mesh = new THREE.Mesh(geom, material);
@@ -204,12 +205,14 @@ class Projectile {
 		mesh.position.y = 0.5 * square;
 		this.mesh = mesh;
 		this.mesh.name = "projectile" + this.random.toString();
+		this.bound = new THREE.Sphere(this.mesh, radius);
 		this.syncMeshToGameLogic();
 		scene.add(mesh);
 	}
 	move() {
-		this.z -= 0.01;
+		this.z -= 0.02;
 		if (!onField(this.x, this.z)) {
+			// Need to also delete this object?
 			this.deleteFromScene();
 		}
 		this.syncMeshToGameLogic();
@@ -218,6 +221,7 @@ class Projectile {
 		var visXZ = logicToVisual(this.x, this.z);
 		this.mesh.position.x = visXZ[0];
 		this.mesh.position.z = visXZ[1];
+		this.bound.center = this.mesh.position;
 	}
 	deleteFromScene() {
 		scene.remove(scene.getObjectByName(this.mesh.name));
@@ -245,17 +249,25 @@ class Centipede {
 			}
 		}
 		this.meshSegments = Array(this.length);
+		this.bounds = [];
+		var radius = 0.6*square;
 		for (var i = 0; i < this.length; i++) {
-			var geom = new THREE.SphereGeometry(0.6*square, 12, 12);
+			var geom = new THREE.SphereGeometry(radius, 12, 12);
 			const material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide});
-			material.color.setHex(0x00ee00);
+			if (i == 0) {
+				material.color.setHex(0xffa500);
+			} else {
+				material.color.setHex(0x00ee00);
+			}
 			this.meshSegments[i] = new THREE.Mesh(geom, material);
 			var visXZ = logicToVisual(this.segments[i][0], this.segments[i][1]);
 			this.meshSegments[i].position.x = visXZ[0];
 			this.meshSegments[i].position.z = visXZ[1];
 			this.meshSegments[i].position.y = 0.5 * square;
-			this.meshSegments[i].name = "Test";
+			this.meshSegments[i].name = "Centipede"+ this.id + "-" + i.toString();
 			scene.add(this.meshSegments[i]);
+			
+			this.bounds.push(new THREE.Sphere(this.meshSegments[i].position, radius));
 		}
 	}
 	syncMeshToGameLogic() {
@@ -263,6 +275,11 @@ class Centipede {
 			var visXZ = logicToVisual(this.segments[i][0], this.segments[i][1]);
 			this.meshSegments[i].position.x = visXZ[0];
 			this.meshSegments[i].position.z = visXZ[1];
+			this.bounds[i].center = this.meshSegments[i].position;
+		}
+		this.meshSegments[0].material.color.setHex(0xffa500);
+		if (this.length > 1) {
+			this.meshSegments[1].material.color.setHex(0x00ee00);
 		}
 	}
 	move(retry) {
@@ -272,11 +289,12 @@ class Centipede {
 			newX = this.segments[0][0];
 			newZ = this.segments[0][1]+1;
 			this.down = false;
-			if (this.direction === 'W') {
+			/*if (this.direction === 'W') {
 				this.direction = 'E';
 			} else {
 				this.direction = 'W';
-			}
+			}*/
+			this.invertDirection();
 		} else if(this.direction === 'W') {
 			newX = this.segments[0][0]-1;
 			newZ = this.segments[0][1];
@@ -295,41 +313,105 @@ class Centipede {
 		}
 		this.syncMeshToGameLogic();
 	}
+	hit(i) {
+		// TODO implement centipede hit
+		// i is the segment that was hit
+		mushrooms.create(this.segments[i][0], this.segments[i][1]);
+		if (i == 0 && this.length == 1) {
+			return [null, null];
+		} else if (i == 0) {
+			var c2 = new Centipede(this.length-i-1, this.inverseDirection(this.direction), this.segments[this.length-1][0], this.segments[this.length-1][1], Math.random().toString());
+			return [c2, null];
+		} else if (i == this.length - 1) {
+			var c1 = new Centipede(i, this.direction, this.segments[0][0], this.segments[0][1], Math.random().toString());
+			return [c1, null];
+		} else {
+			var c1 = new Centipede(i, this.direction, this.segments[0][0], this.segments[0][1], Math.random().toString());
+			var c2 = new Centipede(this.length-i-1, this.direction, this.segments[this.length-1][0], this.segments[this.length-1][1], Math.random().toString());
+			c2.invertDirection();
+			return [c1, c2];
+		}
+	}
+	inverseDirection(dir) {
+		if (dir === 'W') {
+			return 'E';
+		} else if (dir === 'E') {
+			return 'W';
+		}
+	}
+	invertDirection() {
+		this.direction = this.inverseDirection(this.direction);
+	}
+	deleteFromScene() {
+		for (var i = 0; i < this.meshSegments.length; i++) {
+			scene.remove(scene.getObjectByName(this.meshSegments[i].name));
+		}
+	}
 }
 
 class Mushrooms {
 	constructor() {
-		this.map = Array(15);;
+		this.map = Array(15);
+		this.list = [];
+		this.meshes = [];
+		this.bounds = [];
+		this.color = 0xcccccc;
+		this.radius = 0.3*square;
+		this.mushGeom = new THREE.SphereGeometry(this.radius, 12, 12);;
+		this.material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide});
+		this.material.color.setHex(this.color);
+		
 		for(var i = 0; i < 15; i++) {
 			this.map[i] = Array(16);
 			for (var j = 0; j < 16; j++) {
 				this.map[i][j] = 0.0;
 			}
 		}
-		for (var j = 1; j < 16; j++) {
+		for (var j = 1; j < 15; j++) {
 			var randomX = Math.floor(Math.random()*15);
 			if (j == 15 && randomX == 7) {
 				randomX = 8;
 			}
 			this.map[randomX][j] = 4.0;
 		}
-		var mushGeom = new THREE.SphereGeometry(0.3*square, 12, 12);
-		const material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide});
-		material.color.setHex(0xcccccc);
+		
 		for (var i = 0; i < 15; i++) {
 			for (var j = 0; j < 16; j++) {
 				if (this.map[i][j] > 0.0) {
-					const mesh = new THREE.Mesh(mushGeom, material);
+					const mesh = new THREE.Mesh(this.mushGeom, this.material);
 					mesh.rotation.x = Math.PI / 2;
 					mesh.position.y = 0.5 * square;
 					var visXY = logicToVisual(i, j);
 					mesh.position.x = visXY[0];
 					mesh.position.z = visXY[1];
 					scene.add(mesh);
+					this.meshes.push(mesh);
+					
+					var bound = new THREE.Sphere(mesh.position, this.radius);
+					this.bounds.push(bound);
+					this.list.push([i, j]);
 				}
 			}
 		}
 
+	}
+	create(x, z) {
+		// Logical x and z coordinates
+		// Create a new mushroom
+		this.map[x][z] = 4.0;
+		this.list.push([x, z]);
+		const mesh = new THREE.Mesh(this.mushGeom, this.material);
+		mesh.rotation.x = Math.PI / 2;
+		mesh.position.y = 0.5 * square;
+		var visXY = logicToVisual(x, z);
+		mesh.position.x = visXY[0];
+		mesh.position.z = visXY[1];
+		scene.add(mesh);
+		this.meshes.push(mesh);
+		
+		var bound = new THREE.Sphere(mesh.position, this.radius);
+		this.bounds.push(bound);
+		
 	}
 	isIn(x,z) {
 		if (x < 0 || z < 0 || x > 14 || z > 15) {
@@ -337,7 +419,68 @@ class Mushrooms {
 		}
 		return this.map[x][z] > 0.0
 	}
+	hit(i) {
+		// Mushroom i has been hit
+		var x = this.list[i][0];
+		var z = this.list[i][1];
+		if (this.map[x][z] > 0.0) {
+			this.map[x][z] -= 1;
+		}
+		var size = 0.3*square*this.map[x][z] / 4.0
+		this.meshes[i].scale.x = size;
+		this.meshes[i].scale.z = size
+		this.meshes[i].scale.y = size
+	}
 	
+}
+
+function checkProjectileColisions() {
+	var keysForDeletion = [];
+	// Check for mushroom collisions
+	for (var key of projectiles.keys()) {
+		var projectile = projectiles.get(key);
+		for (var i = 0; i < mushrooms.bounds.length; i++) {
+			var potentialCollision = projectile.bound.intersectsSphere(mushrooms.bounds[i]);
+			if (potentialCollision) {
+				var coords = mushrooms.list[i];
+				var value = mushrooms.map[coords[0]][coords[1]];
+				if (value > 0.0) {
+					mushrooms.hit(i);
+					keysForDeletion.push(key);
+				// TODO reduce the mushroom
+				}
+			}
+		}
+		//projectile.bound.intersectsSphere();
+	}
+	// Check for centipede collisions
+	for (var key of projectiles.keys()) {
+		var projectile = projectiles.get(key);
+		for (var j = 0 ; j < centipedes.length; j++) {
+			var centipede = centipedes[j];
+			for (var i = 0; i < centipede.bounds.length; i++) {
+				var collision = projectile.bound.intersectsSphere(centipede.bounds[i]) || centipede.bounds[i].containsPoint(projectile.bound.center);
+				if (collision) {
+					var [c1, c2] = centipede.hit(i);
+					centipedes.splice(j, 1);
+					centipede.deleteFromScene();
+					if (c1 != null) {
+						centipedes.push(c1);
+					}
+					if (c2 != null) {
+						centipedes.push(c2);
+					}
+					keysForDeletion.push(key);
+					break;
+				}
+			}
+		}
+	}
+	for (var i = 0; i < keysForDeletion.length; i++) {
+		projectiles.get(keysForDeletion[i]).deleteFromScene();
+		projectiles.delete(keysForDeletion[i]);
+		// also do cleanup?
+	}
 }
 
 addSolidGeometry(0, 0, -0.5*square, new THREE.PlaneGeometry(15*square, 16*square, 2, 2), 0x11aa44);
@@ -345,7 +488,8 @@ const axes = new THREE.AxesHelper( 15 );
 scene.add(axes);
 const guy = new Guy(7, 15, 0xaa1111);
 const mushrooms = new Mushrooms();
-const centipede = new Centipede(6, 'W', 7, 0, 1);
+var centipedes = [];
+centipedes.push(new Centipede(6, 'W', 7, 0, 1));
 const cpPace = 100;
 var cpCounter = 0;
 var projectiles = new Map();
@@ -362,8 +506,14 @@ const animate = function ( time ) {
 	});*/
 	
 	requestAnimationFrame( animate );
+	
+	checkProjectileColisions();
+	
+	
 	if (cpCounter == cpPace) {
-		centipede.move(true);
+		for (var i = 0; i < centipedes.length; i++) {
+			centipedes[i].move(true);
+		}
 		cpCounter = 0;
 	} else {
 		cpCounter += 1;
@@ -374,6 +524,7 @@ const animate = function ( time ) {
 	for (var projectile of projectiles.values()) {
 		projectile.move();
 	}
+	
 	controls.update();
 	renderer.render( scene, camera );
 };
